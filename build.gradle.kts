e1253626621fc783bf20ce4f2d0ffd621c18f618
TuanWoox <z1venttt@gmail.com>
2025-03-02 16:39:18 +0700

    Finish

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,10 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+buildscript {
+    val agp_version by extra("8.2.0")
+    val agp_version1 by extra("8.7.3")
+}
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.8.0" apply false
-    id("com.android.library") version "8.8.0" apply false
+    id("com.android.application") version "8.7.3" apply false
+    id("com.android.library") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
 }
